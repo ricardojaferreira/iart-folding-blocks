9a0dd20add74d646fbccef2f5d82391b1a548e5e
RicardoFerreira <ricardo.ferreira@blip.pt>
2020-04-07 14:04:45 +0100

    Add javadoc to Statistics::incrementNrNodesToVictory

--- application/src/main/java/pt/up/fe/iart/application/game/Statistics.java
+++ application/src/main/java/pt/up/fe/iart/application/game/Statistics.java
@@ -39,6 +39,9 @@ public class Statistics {
         return nrNodesToVictory;
     }
 
+    /**
+     *
+     */
     public void incrementNrNodesToVictory() {
         this.nrNodesToVictory++;
     }
