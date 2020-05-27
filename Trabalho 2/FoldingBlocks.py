198213bf0a4a4c8a73ebb6ffc593a1511d1f82a7
nunomiguel22 <37627936+nunomiguel22@users.noreply.github.com>
2020-05-27 17:02:40 +0100

    QTable load and save

--- Trabalho 2/FoldingBlocks.py
+++ Trabalho 2/FoldingBlocks.py
@@ -4,8 +4,15 @@ import numpy as np
 import random
 
 
-def qLearning(env, eps, eps_min, eps_decay, episodes, steps, lr, gamma):
-    qtable = np.zeros((300, env.action_space.n))
+def loadQTableCSV(filepath):
+    return np.genfromtxt(filepath, delimiter=',')
+
+
+def saveQTableCSV(qtable, filepath):
+    np.savetxt(filepath, qtable, delimiter=",")
+
+
+def qLearning(env, qtable, eps, eps_min, eps_decay, episodes, steps, lr, gamma):
 
     for ep in range(episodes):
         state = env.reset()
@@ -29,18 +36,24 @@ def qLearning(env, eps, eps_min, eps_decay, episodes, steps, lr, gamma):
             state = new_state
 
             if done:
-                print("Episode finished after {} timesteps".format(step+1))
+                #print("Episode finished after {} timesteps".format(step+1))
                 break
-            print(info)
+            # print(info)
             # env.render()
 
         if eps >= eps_min:
             eps *= eps_decay
 
-        env.render()
+        # env.render()
 
 
-env = gym.make("fb-v1")
-qLearning(env, 1.0, 0.005, 0.99993, 10, 50, 0.65, 0.65)
+env = gym.make("fb-v1")  # Create environment
+
+qtable = np.zeros((300, env.action_space.n))  # Generate new QTable
+# qtable = loadQTableCSV("data/QTable.csv") # Load saved QTable
+
+qLearning(env, qtable, 1.0, 0.005, 0.99993, 1000, 50, 0.65, 0.65)
+
+saveQTableCSV(qtable, "data/QTable.csv")  # Save QTable to file
 
 env.close()
